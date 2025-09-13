16eb14fedf4bd2e9aaac465858663cab4b4137e2
gabrielamandrade <gandrade2@byui.edu>
2025-09-13 20:44:47 -0300

    COMMIT

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,11 @@ if (el) {
   el.textContent = "Last modified: " + document.lastModified;
 }
 
+// Pega o ano atual
+const year = new Date().getFullYear();
+
+// Seleciona o parágrafo que contém o ano
+const p = document.querySelector('footer p');
+
+// Coloca o texto com os emojis no lugar dos traços
+p.innerHTML = `${year} 🍴🥄 Gabriela Magalhães de Andrade 🍳🔪 Brazil`;
